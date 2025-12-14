455b5e4ff6e93b6b19bcf2ad6192157a0f93566e
Laura Muñoz <lm@coondev.com>
2025-12-14 10:29:34 -0500

    [ADD]report and login

--- reporte-financiero/src/main/java/com/api/reporte_financiero/service/ReportService.java
+++ reporte-financiero/src/main/java/com/api/reporte_financiero/service/ReportService.java
@@ -3,6 +3,8 @@ package com.api.reporte_financiero.service;
 import com.api.reporte_financiero.model.Transaction;
 import com.api.reporte_financiero.util.CsvReader;
 import com.api.reporte_financiero.util.PdfGenerator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -11,31 +13,44 @@ import java.util.List;
 @Service
 public class ReportService {
 
-        private final CsvReader csvReader;
-        private final TransactionClassifier classifier;
-        private final PdfGenerator pdfGenerator;
+    private final CsvReader csvReader;
+    private final TransactionClassifier classifier;
+    private final PdfGenerator pdfGenerator;
 
-        public ReportService(CsvReader csvReader, TransactionClassifier classifier, PdfGenerator pdfGenerator) {
-            this.csvReader = csvReader;
-            this.classifier = classifier;
-            this.pdfGenerator = pdfGenerator;
-        }
+    public ReportService(CsvReader csvReader,
+                         TransactionClassifier classifier,
+                         PdfGenerator pdfGenerator) {
+        this.csvReader = csvReader;
+        this.classifier = classifier;
+        this.pdfGenerator = pdfGenerator;
+    }
 
-        // Procesa el archivo CSV y genera el PDF
-        public byte[] processFile(MultipartFile file) {
-            try {
-                //Lee el archivo que se carga y posteriormente se convierte a una lista de transacciones
-                List<Transaction> transactions = csvReader.read(file);
+    public byte[] processFile(MultipartFile file) {
+        try {
+            // Leer CSV
+            List<Transaction> transactions = csvReader.read(file);
 
-                //Clasifica las transacciones
-                List<Transaction> incomes = classifier.filterIncomes(transactions);
-                List<Transaction> selfTransfers = classifier.filterSelfTransfers(transactions);
+            // Clasificar
+            List<Transaction> incomes = classifier.filterIncomes(transactions);
+            List<Transaction> selfTransfers = classifier.filterSelfTransfers(transactions);
 
-                //Genera el PDF con la información organizada
-                return pdfGenerator.generate(incomes, selfTransfers);
+            // Usuario autenticado
+            Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+            String username = auth.getName();
 
-            } catch (Exception e) {
-                throw new RuntimeException("Error al procesar archivo y generar reporte: " + e.getMessage(), e);
-            }
+            // Generar PDF
+            return pdfGenerator.generate(
+                incomes,
+                selfTransfers,
+                transactions,
+                username
+        );
+        
+
+        } catch (Exception e) {
+            throw new RuntimeException(
+                    "Error al procesar archivo y generar reporte: " + e.getMessage(), e
+            );
         }
     }
+}
